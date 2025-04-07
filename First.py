f55d4c81f4cb4503eadab771ab67deabea42d59e
Iliya Saiedi <198695304+IliyaSa06@users.noreply.github.com>
2025-04-08 00:03:52 +0330

    Create First.py
    My first homework

---
+++ First.py
@@ -0,0 +1,43 @@
+#Algebra in python
+#Minus
+# num1 = 50
+# num2 = 3
+
+# num3 = num1 - num2
+# print(num3) #answer= 47
+
+#print('------------------------')
+
+#Multiplied by
+# num1 = 16
+# num2 = 3
+#
+# num3 = num1 * num2
+# print(num3) #answer= 48
+
+#print('------------------------')
+
+#Divided by
+# num1 = 245
+# num2 = 5
+
+# num3 = num1 / num2
+# print(num3) #answer= 49
+
+#print('------------------------')
+
+#squared
+# num1 = 2
+# num2 = 5
+#
+# num3 = num1 ** num2
+# print(num3) #answer= 32
+
+#print('-------------------------')
+
+#Mod
+# num1 = 16
+# num2 = 3
+
+# num3 = num1 // num2
+# print(num3) #answer= 1
